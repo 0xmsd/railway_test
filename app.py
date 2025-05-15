5242760f0a3436e7871fd814e07c456634af3233
melih <melihsuhadal@MSD-MacBook-Air.local>
2025-05-15 22:37:13 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "✅ Railway deployment works!"
+
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
